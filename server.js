44e06a29dc09e5f4865001a54dded70ac2f4de5b
shahriar limon <shahriarwebdev@gmail.com>
2022-10-09 20:15:20 +0600

    connected to database

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const connectDb = require("./Db/db.js")
 const apiRoutes = require("./Routes/apiRoutes")
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use("/api/v1", apiRoutes)
-
+/* mongodb connection */
+connectDb()
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
